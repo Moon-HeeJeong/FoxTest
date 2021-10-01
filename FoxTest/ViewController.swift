6e57aef68cf60858b14433500258078f3de28ad3
JeongMoonHee <ello1010@naver.com>
2021-10-01 14:21:56 +0900

    Commit

--- FoxTest/ViewController.swift
+++ FoxTest/ViewController.swift
@@ -186,6 +186,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        self.navigationItem.title = "video play test"
         TableViewVideo.delegate = self
         TableViewVideo.dataSource = self
         
